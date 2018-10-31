b42837ce2266cac3ef9afd249668045165c50d3d
Jojo <jojoyuji@gmail.com>
2018-10-31 15:59:39 -0400

    thirds in halves command

--- shortcuts/halves.js
+++ shortcuts/halves.js
@@ -3,7 +3,9 @@
 
 const halves = [
   ['[', HYPER, ['half-1']],
-  [']', HYPER, ['half-2']]
+  [']', HYPER, ['half-2']],
+  ['[', HYPER_SHIFT, ['two-third-1']],
+  [']', HYPER_SHIFT, ['two-third-2']]
 ];
 
 setHandlers ( setFrame, halves );
--- shortcuts/thirds.js
+++ shortcuts/thirds.js
@@ -5,8 +5,6 @@ const thirds = [
   [1, HYPER, ['third-1']],
   [2, HYPER, ['third-2']],
   [3, HYPER, ['third-3']],
-  [1, HYPER_SHIFT, ['two-third-1']],
-  [2, HYPER_SHIFT, ['two-third-2']]
 ];
 
 setHandlers ( setFrame, thirds );
